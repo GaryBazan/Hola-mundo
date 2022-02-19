52e1d4881de8987d7e0982ab33fd54640ffc1ac3
GaryBazan <99299725+GaryBazan@users.noreply.github.com>
2022-02-18 23:54:20 -0600

    Add files via upload

---
+++ open.py
@@ -0,0 +1,27 @@
+def main():
+    open("/path/to/mars.jpg")
+
+if __name__ == '__main__':
+    main()
+$ python3 open.py
+Traceback (most recent call last):
+  File "/tmp/open.py", line 5, in <module>
+    main()
+  File "/tmp/open.py", line 2, in main
+    open("/path/to/mars.jpg")
+FileNotFoundError: [Errno 2] No such file or directory: '/path/to/mars.jpg'
+    >>> try:
+...     open('config.txt')
+... except FileNotFoundError:
+...     print("Couldn't find the config.txt file!")
+...
+Couldn't find the config.txt file!
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+
+
+if __name__ == '__main__':
+    main()
